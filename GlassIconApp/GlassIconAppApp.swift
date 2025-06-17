b27096e47bace6fdf5d6a2e090e7236e75554c49
PoojaNegi43 <poojanegi43@gmail.com>
2025-06-17 21:41:11 +0530

    Initial Commit

---
+++ GlassIconApp/GlassIconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassIconAppApp.swift
+//  GlassIconApp
+//
+//  Created by pnkbksh on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassIconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
